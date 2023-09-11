d53df2aecb7b6bc9f66a04a553d0b0d20197728c
Invincibleplus <amazeballst@gmail.com>
2023-09-11 15:57:50 +0200

    Update main.js
    X

--- js/main.js
+++ js/main.js
@@ -142,11 +142,11 @@
         },
       },
     });
-    $("#count").countdown("2023/10/20 02:00:00", function (event) {
+    $("#count").countdown("2023/10/20 12:00:00", function (event) {
       $(this).html(
         event.strftime(
           "" +
-            '<div class="count-block days">%D<span class="count-label">day%!d</span></div>' +
+            '<div class="count-block days">%D<span class="count-label">%!D:day,days;</span></div>' +
             '<div class="count-block hours">%H<span class="count-label">%!H:hour,hours;</span></div>' +
             '<div class="count-block minutes">%M<span class="count-label">%!M:minute,minutes;</span></div>' +
             '<div class="count-block seconds">%S<span class="count-label">%!S:second,seconds;</span></div>'
